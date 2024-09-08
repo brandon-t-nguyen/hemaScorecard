1e6dab85ecf570ca42aa960be443a29466fe7518
Brandon Nguyen <brandon@nguyen.computer>
2024-09-08 01:11:25 -0400

    Add teams object

---
+++ api/v0/model/team.php
@@ -0,0 +1,59 @@
+<?php
+
+require_once __DIR__."/../includes/db.php";
+require_once __DIR__."/person.php";
+
+function get_team($team_id) {
+    $mysqli = db_connector();
+
+    $team = array();
+
+    $sql =
+"
+SELECT
+    teamID,
+    memberName
+FROM
+    eventTeamRoster
+WHERE
+    memberRole = 'teamName' AND teamID = ?
+";
+
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("i", $team_id);
+    $stmt->execute();
+    $result = $stmt->get_result();
+    $row = $result->fetch_row();
+
+    $team['id'] = $row[0];
+    $team['name'] = $row[1];
+
+    // get list of member IDs
+    $members = array();
+    $sql =
+"
+SELECT
+    rosterID
+FROM
+    eventTeamRoster
+WHERE
+    memberRole = 'member' AND teamID = ?
+ORDER BY
+    teamOrder ASC
+";
+    $stmt = $mysqli->prepare($sql);
+    $stmt->bind_param("i", $team_id);
+    $stmt->execute();
+
+    // we will get a list of (event) roster IDs, use each of them to get the appropriate person
+    $result = $stmt->get_result();
+    while ($row = $result->fetch_row()) {
+        $member = get_person_event($row[0]);
+        array_push($members, $member);
+    }
+
+    $team['members'] = $members;
+
+    $mysqli->close();
+    return $team;
+}
---
+++ api/v0/teams.php
@@ -0,0 +1,40 @@
+<?php
+
+require_once "includes/response.php";
+require_once "includes/validation.php";
+
+require_once "model/team.php";
+
+$request = explode('/', trim($_SERVER['PATH_INFO'], '/'));
+$req_method = $_SERVER['REQUEST_METHOD'];
+if (count($request) < 2) {
+    // teams
+    handle_teams();
+} else {
+    // teams/{team.id}
+    $team_id = $request[1];
+    handle_team_id($team_id, array_slice($request, 2));
+}
+
+function handle_teams() {
+    response_todo();
+}
+
+function handle_team_id($team_id, $params) {
+    // /teams/team.id
+    if (count($params) == 0) {
+        $team = get_team($team_id);
+        if (is_null($team)) {
+            response_error("no team of such id", 404);
+        } else {
+            response_success($team);
+        }
+    } else {
+        $method = $params[0];
+        /*
+        if ($method == "exchanges") {
+            response_success(team_get_exchanges($team_id));
+        }
+        */
+    }
+}
